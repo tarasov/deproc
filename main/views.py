0e5fda993d920f7778020f25b6e780eace0853b6
tarasov <tarasov@JustDoIt.(none)>
2012-02-06 10:41:09 +0400

    first view

--- main/views.py
+++ main/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+import logging
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponseServerError, HttpResponse, HttpResponseNotFound
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+def wellcome(request):
+    return render_to_response('tariffication/index.html', locals(), context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from deproc.main.views import wellcome
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^deproc/', include('deproc.foo.urls')),
-
+    (r'^$', wellcome),
     (r'^admin/', include(admin.site.urls)),
 )
