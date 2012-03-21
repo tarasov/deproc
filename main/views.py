4baf4d897586e85f6384f6a91faea9ccfab53520
Pasha Gavrilov <ubunntu@gmail.com>
2012-03-21 12:34:19 +0300

    21.03 - 13.34

--- main/views.py
+++ main/views.py
@@ -15,7 +15,21 @@ def wellcome(request):
     userforms = forms.UserForm()
     bad_models = ['message', u'сайт', u'сессия']
     appmodels = get_models()
+
     tariffs = Tariffication.objects.all()
+    teacher = Tariffication.set_teacher
+    grp = Tariffication.group_plan.group
+    disc = Tariffication.uch_plan_hour.uch_plan.disc
+
+
+
+#        <td align="center">{{ forloop.counter }}</td>
+#        <td align="left"><a href="">{{ tariff.uch_plan_hour.uch_plan.pk }}<b></b></a></td>
+#        <td align="left"><a href="">{{ tariff.set_teacher }}<b></b></a></td>
+#        <td align="left"><a href=""><b>{{ tariff.group_plan.group }}</b></a></td>
+#        <td align="left"><a href=""><b>{{ tariff.uch_plan_hour.uch_plan.disc }}</b></a></td>
+#        <td align="left"><a href=""><b>{{ tariff.uch_plan_hour.type }}</b></a></td>
+#        <td align="left"><a href=""><b>{{ tariff.uch_plan_hour.count_hours}}</b></a></td>
 
     return render_to_response('tariffication/index.html', locals(), context_instance=RequestContext(request))
 
