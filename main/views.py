d04dbc8b948e0d6d7f1477761311b9b146ff6547
Tarasov <tarasov@JustDoIt.(none)>
2012-03-28 13:08:37 +0400

    the table of tariffication complite

--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ from main import models
 from django.db.models import get_models
 from main.forms import DeForm
 from main.models import Tariffication, choice_typeh
+from django.db.models import Count
 
 def wellcome(request):
     choices = choice_typeh
@@ -24,32 +25,76 @@ def wellcome(request):
         if tariff.uch_plan_hour.uch_plan.semestr not in semestres:
             semestres.append(tariff.uch_plan_hour.uch_plan.semestr)
     # делаем цикл по семестрам для фильтрации тариффикации
-    table = tr = ()
+    table = ()
     for semestr in semestres:
-        tariffs = Tariffication.objects.filter(uch_plan_hour__uch_plan__semestr = semestr)
-        tariff = tariffs[0]
-        hours = [(tariff.uch_plan_hour.type, tariff.uch_plan_hour.count_hours) for tariff in tariffs]
-        teacher = tariff.set_teacher.username
-        grp = tariff.group_plan.group
-        disc = tariff.uch_plan_hour.uch_plan.disc
-        smtr = tariff.uch_plan_hour.uch_plan.semestr
-        tr += (teacher,grp,disc,smtr),
-        for typeh in choice_typeh:
-            for i, hour in enumerate(hours):
-                # записываем часы
-                if typeh[0] == hour[0][0]:
-                    choice_type = (typeh[0], hour[1]),
-                    break
-                else:
-                    # тут записываем 0
-                    if i == len(hours)-1:
-                        choice_type = (typeh[0], 0),
-            tr = (tr[len(tr)-1] + choice_type, )
-        table += tr
+#        tariffs = Tariffication.objects.values('group_plan__group__name').annotate(count = Count('group_plan__group')).filter(uch_plan_hour__uch_plan__semestr = semestr)
+        tariffs_values = Tariffication.objects.values('group_plan__group', 'teacher', 'uch_plan_hour__uch_plan__semestr').annotate(count = Count('group_plan__group')).filter(uch_plan_hour__uch_plan__semestr = semestr)
+        tr = tds = ()
+        for value in tariffs_values:
+#            print value
+            tariffs = Tariffication.objects.filter(uch_plan_hour__uch_plan__semestr = value['uch_plan_hour__uch_plan__semestr'], teacher = value['teacher'], group_plan__group = value['group_plan__group'])
+#            print tariff
+#            tariff = tariffs[0]
+            tariff = tariffs[0]
+            hours = [(t.uch_plan_hour.type, t.uch_plan_hour.count_hours) for t in tariffs]
+            teacher = tariff.set_teacher.username
+            grp = tariff.group_plan.group
+            disc = tariff.uch_plan_hour.uch_plan.disc
+            smtr = tariff.uch_plan_hour.uch_plan.semestr
+            tr += (teacher,grp,disc,smtr),
+            for typeh in choice_typeh:
+                for i, hour in enumerate(hours):
+                    # записываем часы
+                    if typeh[0] == hour[0][0]:
+                        choice_type = (typeh[0], hour[1]),
+                        break
+                    else:
+                        # тут записываем 0
+                        if i == len(hours)-1:
+                            choice_type = (typeh[0], 0),
+                tr = (tr[len(tr)-1] + choice_type, )
+            table += tr
     return render_to_response('tariffication/index.html', locals(), context_instance=RequestContext(request))
 
 def tarification(request):
     form = forms.TarificationForm()
+    # отрисовка таблицы
+    tariffs = Tariffication.objects.all()
+    # узнаем семестры
+    semestres = []
+    for tariff in tariffs:
+        if tariff.uch_plan_hour.uch_plan.semestr not in semestres:
+            semestres.append(tariff.uch_plan_hour.uch_plan.semestr)
+        # делаем цикл по семестрам для фильтрации тариффикации
+    table = ()
+    for semestr in semestres:
+    #        tariffs = Tariffication.objects.values('group_plan__group__name').annotate(count = Count('group_plan__group')).filter(uch_plan_hour__uch_plan__semestr = semestr)
+        tariffs_values = Tariffication.objects.values('group_plan__group', 'teacher', 'uch_plan_hour__uch_plan__semestr').annotate(count = Count('group_plan__group')).filter(uch_plan_hour__uch_plan__semestr = semestr)
+        tr = tds = ()
+        for value in tariffs_values:
+        #            print value
+            tariffs = Tariffication.objects.filter(uch_plan_hour__uch_plan__semestr = value['uch_plan_hour__uch_plan__semestr'], teacher = value['teacher'], group_plan__group = value['group_plan__group'])
+            #            print tariff
+            #            tariff = tariffs[0]
+            tariff = tariffs[0]
+            hours = [(t.uch_plan_hour.type, t.uch_plan_hour.count_hours) for t in tariffs]
+            teacher = tariff.set_teacher.username
+            grp = tariff.group_plan.group
+            disc = tariff.uch_plan_hour.uch_plan.disc
+            smtr = tariff.uch_plan_hour.uch_plan.semestr
+            tr += (teacher,grp,disc,smtr),
+            for typeh in choice_typeh:
+                for i, hour in enumerate(hours):
+                    # записываем часы
+                    if typeh[0] == hour[0][0]:
+                        choice_type = (typeh[0], hour[1]),
+                        break
+                    else:
+                        # тут записываем 0
+                        if i == len(hours)-1:
+                            choice_type = (typeh[0], 0),
+                tr = (tr[len(tr)-1] + choice_type, )
+            table += tr
     return render_to_response('tariffication/tariffication.html', locals(), context_instance=RequestContext(request))
 
 def action(request, app):
