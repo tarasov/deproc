210a3c3a29288f832181db80d005a3e23cf12272
Ruslan Tarasov <ruslan.tarasov2@gmail.com>
2012-05-18 23:57:18 +0400

    команда еще не работает...

--- deproc/tariffication/management/commands/update_empty_typehour_for_tariffication.py
+++ deproc/tariffication/management/commands/update_empty_typehour_for_tariffication.py
@@ -1,5 +1,5 @@
-__author__ = 'tarasov'
 # -*- coding: utf-8 -*-
+from deproc.tariffication.models import Tariffication
 from django.core.management.base import BaseCommand
 from django.db import transaction, connection
 from django.db.models import Q
@@ -14,15 +14,27 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         tariffications = models.Tariffication.objects.all().select_related('uch_plan_hour', 'uch_plan_hour__uch_plan')
-
+        teachers = models.Teachers.objects.all()
         logging.info('tariffications: {0}'.format(tariffications.count()))
+        typehours = models.TypeHour.objects.all()
 
-        for tariffication in tariffications:
-            try:
-                logging.info('START {0}'.format(tariffication))
-                transaction.commit()
-                logging.info('END {0}'.format(tariffication))
 
-            except Exception as e:
-                logging.info('ERROR {0}; {1}'.format(tariffication, str(e)))
-                transaction.rollback()
+        for teacher in teachers:
+            for group in teacher.get_groups():
+                for discipline in group.get_disciplines(teacher):
+                    try:
+                        for typehour in typehours:
+                            obj, is_created = Tariffication.objects.get_or_create(
+                                teacher = teacher,
+                                group_plan__group = group,
+                                uch_plan_hour__uch_plan__disc = discipline,
+                                uch_plan_hour__type_hour__name = typehour,
+                                defaults = {'type_hour': 0}
+                            )
+                        if is_created:
+                            transaction.commit()
+                        else:
+                            transaction.rollback()
+                    except Exception as e:
+                        logging.error('ERROR {0}; {1}'.format('ERROR!!!', str(e)))
+                        transaction.rollback()
